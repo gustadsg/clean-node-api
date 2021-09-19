fda3c2c20583068d96f283feea027d9a0768f81d
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-19 09:14:35 -0300

    refactor: move sut creation to a helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,11 @@
 import { SignUpController } from "./signup";
 import { MissingParamError } from "../errors/missing-param-error";
 
+const makeSut = () => new SignUpController();
+
 describe("SignUp Controller", () => {
   test("should return 400 if no name is provided", () => {
-    const sut = new SignUpController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         // name: "any_name",
@@ -19,7 +21,7 @@ describe("SignUp Controller", () => {
   });
 
   test("should return 400 if no email is provided", () => {
-    const sut = new SignUpController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         name: "any_name",
@@ -35,7 +37,7 @@ describe("SignUp Controller", () => {
   });
 
   test("should return 400 if no password is provided", () => {
-    const sut = new SignUpController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         name: "any_name",
