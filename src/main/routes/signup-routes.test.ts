2d97a592db2a4f115fd9961e0f849f12ab10297b
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-21 09:07:35 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,8 +1,23 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from "supertest";
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mogo-helper";
 import app from "../config/app";
 
 describe("Signup Routes", () => {
+  beforeAll(async () => {
+    const mongoURL = process.env.MONGO_URL || "";
+    await MongoHelper.connect(mongoURL);
+  });
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getConnection("accounts");
+    await accountCollection?.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
   test("should return 200 on success", async () => {
     await request(app)
       .post("/api/signup")
