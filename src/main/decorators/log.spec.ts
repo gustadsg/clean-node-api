fa2d8cee57a8509ee09ad6eeee56d80bbb9833b1
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-22 10:02:03 -0300

    refactor: move controller creation to a factory helper method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -10,7 +10,7 @@ interface SutTypes {
   sut: LogControllerDecorator;
 }
 
-const makeSut = (): SutTypes => {
+const makeController = (): Controller => {
   class ControllerStub implements Controller {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -22,7 +22,11 @@ const makeSut = (): SutTypes => {
     }
   }
 
-  const controllerStub = new ControllerStub();
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController();
   const sut = new LogControllerDecorator(controllerStub);
 
   return { controllerStub, sut };
