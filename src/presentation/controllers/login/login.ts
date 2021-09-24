2852b57c94cc4a7e3dc4c8dd153b30518144d306
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-24 08:43:18 -0300

    feat: ensure LoginController calls email validator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { MissingParamError } from "../../errors";
 import { badRequest, ok } from "../../helpers/http-helper";
+import { EmailValidator } from "../signup/signup-protocols";
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
@@ -15,6 +22,7 @@ export class LoginController implements Controller {
         resolve(badRequest(new MissingParamError("password")))
       );
     }
+    this.emailValidator.isValid(httpRequest.body.email);
     return new Promise((resolve) => resolve(ok("ok")));
   }
 }
