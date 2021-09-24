3e498bbdb8ef5a7f65da8a2594a52783732b6760
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-24 08:44:02 -0300

    test: ensure LoginController calls email validator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,15 +1,28 @@
 import { LoginController } from "./login";
 import { badRequest } from "../../helpers/http-helper";
 import { MissingParamError } from "../../errors";
+import { EmailValidator } from "../signup/signup-protocols";
 
 interface SutTypes {
   sut: LoginController;
+  emailValidatorStub: EmailValidator;
 }
 
-const makeSut = (): SutTypes => {
-  const sut = new LoginController();
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid() {
+      return true;
+    }
+  }
 
-  return { sut };
+  return new EmailValidatorStub();
+};
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new LoginController(emailValidatorStub);
+
+  return { sut, emailValidatorStub };
 };
 
 describe("Login Controller", () => {
@@ -24,6 +37,7 @@ describe("Login Controller", () => {
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
   });
+
   test("should return 400 if no password is provided", async () => {
     const { sut } = makeSut();
     const httpRequest = {
@@ -34,5 +48,21 @@ describe("Login Controller", () => {
     const httpResponse = await sut.handle(httpRequest);
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+  });
+
+  test("should call email validator with correct email", async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        password: "any_password",
+      },
+    };
+
+    await sut.handle(httpRequest);
+
+    expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
 });
