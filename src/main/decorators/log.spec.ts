615739f257c5f21de11daad54acbb1d4ae231e36
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-23 22:11:45 -0300

    refactor: remove duplicated code

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -6,8 +6,9 @@ import {
   HttpResponse,
 } from "../../presentation/protocols";
 import { LogControllerDecorator } from "./log";
-import { serverError } from "../../presentation/helpers/http-helper";
+import { serverError, created } from "../../presentation/helpers/http-helper";
 import { LogErrorRepository } from "../../data/protocols/log-error-repository";
+import { AccountModel } from "../../domain/models/account";
 
 interface SutTypes {
   controllerStub: Controller;
@@ -15,15 +16,32 @@ interface SutTypes {
   logErrorRepositoryStub: LogErrorRepository;
 }
 
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: "any_name",
+    email: "any_email@email.com",
+    password: "any_password",
+    passwordConfirmation: "any_password",
+  },
+});
+
+const makeFakeAccount = (): AccountModel => ({
+  id: "valid_id",
+  name: "valid_name",
+  email: "valid_email",
+  password: "valid_password",
+});
+
+const makeFakeServerError = (): HttpResponse => {
+  const fakeError = new Error();
+  fakeError.stack = "any_stack";
+  return serverError(fakeError);
+};
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-      const httpResponse: HttpResponse = {
-        statusCode: 200,
-        body: { name: "gustavo" },
-      };
-      return new Promise((resolve) => resolve(httpResponse));
+      return new Promise((resolve) => resolve(created(makeFakeAccount())));
     }
   }
 
@@ -54,14 +72,7 @@ describe("LogController Decorator", () => {
     const { sut, controllerStub } = makeSut();
     const handleSpy = jest.spyOn(controllerStub, "handle");
 
-    const httpRequest = {
-      body: {
-        name: "any_name",
-        email: "any_email@mail.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
-    };
+    const httpRequest = makeFakeRequest();
 
     await sut.handle(httpRequest);
     expect(handleSpy).toBeCalledWith(httpRequest);
@@ -69,40 +80,23 @@ describe("LogController Decorator", () => {
 
   test("should return the same result of the controller handle method", async () => {
     const { sut } = makeSut();
-
-    const httpRequest = {
-      body: {
-        name: "any_name",
-        email: "any_email@mail.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
-    };
-
+    const httpRequest = makeFakeRequest();
     const sutResponse = await sut.handle(httpRequest);
-    expect(sutResponse).toEqual({
-      statusCode: 200,
-      body: { name: "gustavo" },
-    });
+
+    expect(sutResponse).toEqual(created(makeFakeAccount()));
   });
 
   test("should call LogErrorRepository with correct error if controller returns a server error", async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
-    const fakeError = new Error();
-    fakeError.stack = "any_stack";
-    const error = serverError(fakeError);
+
     const logSpy = jest.spyOn(logErrorRepositoryStub, "log");
+
     jest
       .spyOn(controllerStub, "handle")
-      .mockReturnValueOnce(new Promise((resolve) => resolve(error)));
-    const httpRequest = {
-      body: {
-        name: "any_name",
-        email: "any_email@mail.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
-    };
+      .mockReturnValueOnce(
+        new Promise((resolve) => resolve(makeFakeServerError()))
+      );
+    const httpRequest = makeFakeRequest();
 
     await sut.handle(httpRequest);
     expect(logSpy).toBeCalledWith("any_stack");
