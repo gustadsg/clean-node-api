c7bd8c259a6579564d708c345a4a5208658bebb5
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-23 21:36:27 -0300

    feat: ensure controller calls LogErrorRepository with correct values if it returns an error

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,16 +1,22 @@
+import { LogErrorRepository } from "../../data/protocols/log-error-repository";
 import { Controller, HttpRequest } from "../../presentation/protocols";
 
 export class LogControllerDecorator implements Controller {
   private readonly controller: Controller;
 
-  constructor(controller: Controller) {
+  private readonly logErrorRepository: LogErrorRepository | null = null;
+
+  constructor(controller: Controller, logErrorRepository?: LogErrorRepository) {
     this.controller = controller;
+    if (logErrorRepository) {
+      this.logErrorRepository = logErrorRepository;
+    }
   }
 
   async handle(httpRequest: HttpRequest) {
     const httpResponse = await this.controller.handle(httpRequest);
-    if (httpResponse.statusCode === 500) {
-      // log
+    if (httpResponse.statusCode === 500 && this.logErrorRepository) {
+      this.logErrorRepository.log(httpResponse.body.stack);
     }
     return httpResponse;
   }
