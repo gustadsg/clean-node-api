8172bcfab3a950dfd92b1ba98d1e839fac3ffb7c
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-23 22:42:18 -0300

    feat: ensure LogMongoRepository creates an error log on success

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -16,7 +16,7 @@ export class LogControllerDecorator implements Controller {
   async handle(httpRequest: HttpRequest) {
     const httpResponse = await this.controller.handle(httpRequest);
     if (httpResponse.statusCode === 500 && this.logErrorRepository) {
-      this.logErrorRepository.log(httpResponse.body.stack);
+      this.logErrorRepository.logError(httpResponse.body.stack);
     }
     return httpResponse;
   }
