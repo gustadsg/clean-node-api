543cf3c84927b945e1349ca282aae08fe2c82ced
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-24 14:03:57 -0300

    feat: ensure LoginController calls Authentication with correct valuess

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -2,12 +2,16 @@ import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { badRequest, ok, serverError } from "../../helpers/http-helper";
 import { EmailValidator } from "../signup/signup-protocols";
+import { Authentication } from "../../../domain/usecases/authentication";
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator;
 
-  constructor(emailValidator: EmailValidator) {
+  private readonly authentication: Authentication;
+
+  constructor(emailValidator: EmailValidator, authentication: Authentication) {
     this.emailValidator = emailValidator;
+    this.authentication = authentication;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -33,6 +37,8 @@ export class LoginController implements Controller {
           resolve(badRequest(new InvalidParamError("email")))
         );
       }
+
+      await this.authentication.auth(email, password);
 
       return new Promise((resolve) => resolve(ok("ok")));
     } catch (error) {
