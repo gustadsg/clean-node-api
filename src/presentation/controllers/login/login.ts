b918844db881a1e419294aaecef317fd36e7154c
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-24 08:31:33 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,12 +1,20 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { MissingParamError } from "../../errors";
-import { badRequest } from "../../helpers/http-helper";
+import { badRequest, ok } from "../../helpers/http-helper";
 
 export class LoginController implements Controller {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return new Promise((resolve) =>
-      resolve(badRequest(new MissingParamError("email")))
-    );
+    if (!httpRequest.body.email) {
+      return new Promise((resolve) =>
+        resolve(badRequest(new MissingParamError("email")))
+      );
+    }
+    if (!httpRequest.body.password) {
+      return new Promise((resolve) =>
+        resolve(badRequest(new MissingParamError("password")))
+      );
+    }
+    return new Promise((resolve) => resolve(ok("ok")));
   }
 }
