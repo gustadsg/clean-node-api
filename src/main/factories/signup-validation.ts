f58250c39d0782679aabc29e13fc28fc87bba188
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-26 13:04:37 -0300

    feat: ensure SignUpController calls Validation with correct values

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,7 +1,15 @@
+import { RequiredFieldValidation } from "../../presentation/helpers/validators/required-field-validation";
 import { Validation } from "../../presentation/helpers/validators/validation";
 import { ValidationComposite } from "../../presentation/helpers/validators/validation-composite";
 
 export const makeSignupValidation = (): Validation => {
-  const validationComposite = new ValidationComposite([]);
+  const requiredFields = ["name", "email", "password", "passwordConfirmation"];
+  const validations: Validation[] = [];
+
+  for (const field of requiredFields) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+
+  const validationComposite = new ValidationComposite(validations);
   return validationComposite;
 };
