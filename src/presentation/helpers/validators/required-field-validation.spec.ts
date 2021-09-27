eda54ef1f4bf954f26652a00c60e0c5a74c6ce81
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-26 22:10:59 -0300

    test: ensure RequiredFieldValidator returns an Error when required field is missing

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import faker from "faker";
+import { MissingParamError } from "../../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 
 const field = faker.random.word();
@@ -8,6 +9,13 @@ const makeSut = (): RequiredFieldValidation =>
   new RequiredFieldValidation(field);
 
 describe("RequiredFieldValidation", () => {
+  test("should return an error if required field is missing", () => {
+    const sut = makeSut();
+    const isValid = sut.validate({ InvalidField: faker.random.word() });
+
+    expect(isValid).toEqual(new MissingParamError(field));
+  });
+
   test("should return null if required field is provided", () => {
     const sut = makeSut();
     const isValid = sut.validate({ [field]: faker.random.word() });
