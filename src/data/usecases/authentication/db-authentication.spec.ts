98f3c82b1487ba2b8f7344922995d28677b100bf
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-30 10:10:50 -0300

    refactor: move duplicated code to helper factory methods

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -3,31 +3,54 @@ import faker from "faker";
 import { AccountModel } from "../add-account/db-add-account-protocols";
 import { LoadAccountByEmailRepository } from "../../protocols/load-account-by-email-repository";
 import { DbAuthentication } from "./db-authentication";
+import { AuthenticationModel } from "../../../domain/usecases/authentication";
+
+interface SutTypes {
+  sut: DbAuthentication;
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
+}
+
+const makeFakeAccount = (): AccountModel => ({
+  id: "any_id",
+  name: "anmy_name",
+  email: "any_email@mail.com",
+  password: "any_password",
+});
+
+const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub
+    implements LoadAccountByEmailRepository
+  {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async load(email: string): Promise<AccountModel> {
+      const account = makeFakeAccount();
+      return new Promise((resolve) => resolve(account));
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub();
+};
+
+const makeFakeAuthentication = (): AuthenticationModel => ({
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+});
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository();
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub);
+
+  return {
+    loadAccountByEmailRepositoryStub,
+    sut,
+  };
+};
 
 describe("DbAuthentication Usecase", () => {
   test("should call LoadAccountByEmailRepository with correct email", () => {
-    class LoadAccountByEmailRepositoryStub
-      implements LoadAccountByEmailRepository
-    {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async load(email: string): Promise<AccountModel> {
-        const account = {
-          id: "any_id",
-          name: "any_name",
-          email: "any_email@mail.com",
-          password: "any_password",
-        };
-        return new Promise((resolve) => resolve(account));
-      }
-    }
-    const loadAccountByEmailRepositoryStub =
-      new LoadAccountByEmailRepositoryStub();
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub);
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, "load");
-    const payload = {
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
+    const payload = makeFakeAuthentication();
+
     sut.auth(payload);
     expect(loadSpy).toHaveBeenCalledWith(payload.email);
   });
