b804b4f214621510c080a3bfa62bd133498175ad
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-09-30 10:14:44 -0300

    test: ensure DbAuthentication throws if LoadAccountByEmailRepository throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -54,4 +54,15 @@ describe("DbAuthentication Usecase", () => {
     sut.auth(payload);
     expect(loadSpy).toHaveBeenCalledWith(payload.email);
   });
+
+  test("should throw if LoadAccountByEmailRepository throws", async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadAccountByEmailRepositoryStub, "load")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const promise = sut.auth(makeFakeAuthentication());
+    expect(promise).rejects.toThrow();
+  });
 });
