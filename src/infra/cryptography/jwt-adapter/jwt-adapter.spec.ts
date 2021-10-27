6ab3110071eb9321d3bb9a6789ca5e62cc2ae057
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-10-27 13:00:28 -0300

    test: ensure JwtAdapter returns a token when sign succeeds

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { random } from "faker";
 import Jwt from "jsonwebtoken";
 import { JwtAdapter } from "./jwt-adapter";
 
+const randomToken = random.alphaNumeric(64);
+const randomId = random.alphaNumeric(32);
+const randomSecret = random.alphaNumeric(32);
+
+jest.mock("jsonwebtoken", () => ({
+  sign: async () => new Promise((resolve) => resolve(randomToken)),
+}));
+
 describe("Jwt Adapter", () => {
   test("Should call sign with correct values", async () => {
-    const sut = new JwtAdapter("secret");
+    const sut = new JwtAdapter(randomSecret);
     const signSpy = jest.spyOn(Jwt, "sign");
-    await sut.encrypt("any_id");
-    expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
+    await sut.encrypt(randomId);
+    expect(signSpy).toHaveBeenCalledWith({ id: randomId }, randomSecret);
+  });
+
+  test("Should return a token on sign success", async () => {
+    const sut = new JwtAdapter(randomSecret);
+    const accessToken = await sut.encrypt(randomId);
+    expect(accessToken).toBe(randomToken);
   });
 });
