841950dbef2abc28f000d1e65b182a580d9a57f4
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-10-28 14:28:20 -0300

    feat: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,17 @@
+import { ObjectId } from "mongodb";
 import { AddAccountRepository } from "../../../../data/protocols/db/add-account-repository";
 import { AddAccountModel } from "../../../../domain/usecases/add-account";
 import { AccountModel } from "../../../../domain/models/account";
 import { MongoHelper } from "../helpers/mogo-helper";
+import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load-account-by-email-repository";
+import { UpdateAccessTokenRepository } from "../../../../data/protocols/db/update-access-token-repository";
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements
+    AddAccountRepository,
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenRepository
+{
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountColletcion = await MongoHelper.getConnection("accounts");
 
@@ -39,5 +47,18 @@ export class AccountMongoRepository implements AddAccountRepository {
     const account = MongoHelper.map(foundObj);
 
     return account;
+  }
+
+  async updateAccessToken(id: string | ObjectId, token: string): Promise<void> {
+    const accountColletcion = await MongoHelper.getConnection("accounts");
+
+    if (!accountColletcion) {
+      throw new Error("account collection does not exist");
+    }
+
+    await accountColletcion.updateOne(
+      { _id: id },
+      { $set: { accessToken: token } }
+    );
   }
 }
