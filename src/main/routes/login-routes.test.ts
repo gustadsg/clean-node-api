5525af44009395ef1294f24abb544e69b0a0e097
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-10-31 14:01:55 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,8 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import request from "supertest";
 import faker from "faker";
+import { Collection } from "mongodb";
+import { hash } from "bcrypt";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mogo-helper";
 import app from "../config/app";
+
+let accountCollection: Collection | null;
 
 describe("Login Routes", () => {
   beforeAll(async () => {
@@ -11,13 +15,14 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getConnection("accounts");
+    accountCollection = await MongoHelper.getConnection("accounts");
     await accountCollection?.deleteMany({});
   });
 
   afterAll(async () => {
     await MongoHelper.disconnect();
   });
+
   describe("POST /signup", () => {
     test("should return 200 on signup success", async () => {
       const fakerPass = faker.internet.password();
@@ -31,6 +36,26 @@ describe("Login Routes", () => {
           passwordConfirmation: fakerPass,
         })
         .expect(201);
+    });
+  });
+
+  describe("POST /login", () => {
+    test("should return 200 on login success", async () => {
+      const userInfo = {
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      };
+
+      await accountCollection?.insertOne({
+        ...userInfo,
+        password: await hash(userInfo.password, 12),
+      });
+
+      await request(app)
+        .post("/api/login")
+        .send({ email: userInfo.email, password: userInfo.password })
+        .expect(200);
     });
   });
 });
