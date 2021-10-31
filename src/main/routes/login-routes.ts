6446db910162801ce018ce6016e3c96d4f308207
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-10-31 14:01:38 -0300

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { makeSignUpController } from "../factories/signup/signup-factory";
 import { adaptRoute } from "../adapters/express/express-route-adapter";
+import { makeLoginController } from "../factories/login/login-factory";
 
 export default (router: Router): void => {
-  const signupController = makeSignUpController();
-  router.post("/signup", adaptRoute(signupController));
+  router.post("/signup", adaptRoute(makeSignUpController()));
+  router.post("/login", adaptRoute(makeLoginController()));
 };
