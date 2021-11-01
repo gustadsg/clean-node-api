7eeb1153145adfd9e96f0048450838d06266ff81
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-11-01 13:13:35 -0300

    refactor: change routes to use fs intead of fast-glob

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,20 @@
 import { Express, Router } from "express";
-import fg from "fast-glob";
+import { readdirSync } from "fs";
+import { resolve } from "path";
 
 export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
 
-  fg.sync("**/src/main/routes/**routes.ts").map(async (file) => {
-    const route = await require(`../../../${file}`).default;
-    route(router);
+  const routesPath = resolve(__dirname, "..", "routes");
+
+  readdirSync(routesPath).map(async (file) => {
+    if (!isTestFile(file)) {
+      (await import(resolve(routesPath, file))).default(router);
+    }
   });
 };
+
+function isTestFile(path: string): boolean {
+  return path.includes(".test.") || path.includes(".spec.");
+}
