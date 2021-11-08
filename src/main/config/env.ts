e79aabfa149019d20e1a846677cfd3a7b957320b
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-11-08 12:43:59 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoURL: process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
+  mongoURL: process.env.MONGO_URL || "mongodb://mogo:27017/clean-node-api",
   port: process.env.PORT || 5050,
   jwtSecret:
     process.env.JWT_SECRET ||
